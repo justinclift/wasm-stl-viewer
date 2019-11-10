0a02b1e304e06bdb22dd5a477d31e4a5f2470130
Justin Clift <justin@postgresql.org>
2019-11-10 22:04:15 +1100

    Add known good server.go

--- server.go
+++ server.go
@@ -1,42 +1,25 @@
 package main
 
 import (
-	"io/ioutil"
+	"fmt"
 	"log"
 	"net/http"
-	"runtime"
+	"strings"
+)
+
+const (
+	dir  = "./docs"
+	port = 8080
 )
 
 func main() {
-	indexData, err := ioutil.ReadFile("index.html")
-	if err != nil {
-		log.Fatalf("Could not read index file: %s\n", err)
-	}
-
-	wasmExecLocation := runtime.GOROOT() + "/misc/wasm/wasm_exec.js"
-	wasmExecData, err := ioutil.ReadFile(wasmExecLocation)
-	if err != nil {
-		log.Fatalf("Could not read wasm_exec file: %s\n", err)
-	}
-
-	wasmData, err := ioutil.ReadFile("bundle.wasm")
-	if err != nil {
-		log.Fatalf("Could not read wasm file: %s\n", err)
-	}
-
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write(indexData)
-	})
-
-	http.HandleFunc("/wasm_exec.js", func(w http.ResponseWriter, r *http.Request) {
-		w.Write(wasmExecData)
-	})
-
-	http.HandleFunc("/bundle.wasm", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "application/wasm")
-		w.WriteHeader(http.StatusOK)
-		w.Write(wasmData)
-	})
-
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	fs := http.FileServer(http.Dir(dir))
+	log.Printf("Serving %s on http://localhost:%d", dir, port)
+	http.ListenAndServe(fmt.Sprintf(":%d", port), http.HandlerFunc(func(resp http.ResponseWriter, req *http.Request) {
+		resp.Header().Add("Cache-Control", "no-cache")
+		if strings.HasSuffix(req.URL.Path, ".wasm") {
+			resp.Header().Set("content-type", "application/wasm")
+		}
+		fs.ServeHTTP(resp, req)
+	}))
 }
